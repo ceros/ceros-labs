9df9ab11f3f74a926bc15b3f446c6571d6caf261
Brennan Pilcher <brennan.pilcher@ceros.com>
2018-02-07 13:37:00 -0500

    Updated constants.js in the wizard to reflect the new version URL.

--- standalone-redirect/wizard/src/modules/constants.js
+++ standalone-redirect/wizard/src/modules/constants.js
@@ -1,4 +1,4 @@
-export const URL_PLUGIN_SRC = '//labs.ceros.com/standalone-redirect/v0-9-1/main.min.js';
+export const URL_PLUGIN_SRC = '//labs.ceros.com/standalone-redirect/v0-9-2/main.min.js';
 
 export const BREAK_TYPE_TOUCH_EVENTS = "BREAK_TYPE_TOUCH";
 export const BREAK_TYPE_USER_AGENT = "BREAK_TYPE_USER_AGENT";
